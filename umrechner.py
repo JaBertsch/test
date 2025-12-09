b7cb435527a8a8c6746be67f5ba570214f5db800
JaBertsch <m25008@hb.dhbw-stuttgart.de>
2025-12-09 13:03:11 +0000

    Füge Kilometerumrechner und Testvariablen hinzu

---
+++ umrechner.py
@@ -0,0 +1,7 @@
+print("Willkommen beim Kilometerumrechner")
+#Eingabe von Kilometerstand
+meter = int (input("Gib deinen Meterwert ein"))
+message = f"Deine Eingabe waren: {meter} m"
+print(message)
+kilometer = meter / 1000
+print(f"Die konvertierte Meterzahl ist {kilometer}")

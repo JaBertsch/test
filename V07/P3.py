3ea69c25c0a2dc52014014fd235beea2f47b1152
JaBertsch <m25008@hb.dhbw-stuttgart.de>
2026-02-09 17:13:53 +0000

    Tabelle ergänzt

--- V07/P3.py
+++ V07/P3.py
@@ -4,12 +4,22 @@ nenndurchmesser_schrauben = [3,4,5,6,8,10]
 spannungsquerschnitt = 0.8 * (3.14*nenndurchmesser_schrauben[0]**2)/4
 
 zugfestigkeiten = [400, 800, 1000]
-vorspannkraft = 0.2 * nenndurchmesser_schrauben * spannungsquerschnitt * zugfestigkeiten[0]
+vorspannkraft = 0.7 * spannungsquerschnitt * zugfestigkeiten[0]
 
-anziehdrehmoment = 0.2 * nenndurchmesser_schrauben * vorspannkraft
+anziehdrehmoment = 0.2 * nenndurchmesser_schrauben[0] * vorspannkraft
 
+print("=" * 47)
+print("Anziehdrehmoment-Tabelle")
+print("=" * 47)
+
+print(f"{"Gewinde":<8}|", end="")
+for zugfestigkeit in zugfestigkeiten:
+    print(f"{zugfestigkeit:>9} [Nm] |", end="")
+
+print()
+print("─" * 8 + "|" + "─" * 11 + "|" + "─" * 11 + "|" + "─" * 11 + "|")
 
 for durchmesser in nenndurchmesser_schrauben:
-    print(f"M{durchmesser}     |", end="")
+    print(f"M{durchmesser}      |", sep="")
 
-    #for zugfestigkeit in zugfestigkeiten:
+

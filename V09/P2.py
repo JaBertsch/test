d1c83b89d2c932c6bd2fe60cad27e13c9e448156
JaBertsch <m25008@hb.dhbw-stuttgart.de>
2026-02-24 14:12:34 +0000

    Füge Log-Datenanalyse hinzu: Zähle ALARM- und ERROR-Ereignisse in der Log-Datei

--- V09/P2.py
+++ V09/P2.py
@@ -1,5 +1,14 @@
 datei = open("V09/maschine_01.log")
 zeilen = datei.readlines()
+print(f"Anzahl Zeilen: {len(zeilen)}")
+anzahlAlarm = 0
+anzahlError = 0
 
 for zeile in zeilen:
-    print(zeile)
+    anzahlAlarm += zeile.count("ALARM")
+    anzahlError += zeile.count("ERROR")
+
+print(f"anzahl alarm: {anzahlAlarm}")
+print(f"Anzahl error: {anzahlError}")
+
+zeile.

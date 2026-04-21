753d61d31c61d41004085f0f872d79b5ccb1cc34
JaBertsch <m25008@hb.dhbw-stuttgart.de>
2026-04-21 08:19:39 +0000

    Aktualisiere Bool.py, um den Sensorwert und seinen Typ anzuzeigen; füge die Ausgabe als Integer, Float und Boolean hinzu

--- Wiederholung/Bool.py
+++ Wiederholung/Bool.py
@@ -1,5 +1,12 @@
-Sensor_Nachricht = print(input())
-print(f"Ursrünglicher Wert: {Sensor_Nachricht} ", end="  ")
-print(f"Typ: {type(Sensor_Nachricht)}")
-print(f"Als Integer: {int(Sensor_Nachricht)}", end="  ")
-#print(f"Typ: {type(Sensor_Nachricht)}")
+sensor_wert = (input("empfangene Nachricht: "))
+print(f"Ursrünglicher Wert: {sensor_wert} ", end="  ")
+print(f"Typ: {type(sensor_wert)}")
+
+wert_int = int(sensor_wert)
+print(f"Als Integer: {wert_int} ,  Typ: {type(wert_int)} ,  Drehzahl: {wert_int} U/min")
+
+wert_float = float(sensor_wert)
+print(f"Als Float: {wert_float} ,  Typ: {type(wert_float)} ,  Temperatur: {wert_float} °C")
+
+wert_bool = bool(sensor_wert)
+print(f"Als Boolean: {wert_bool},  Typ: {type(wert_bool)} ,  Motor: LÄUFT ")
